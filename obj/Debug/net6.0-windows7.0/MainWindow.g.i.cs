6cf6e204b84e6cb577767320c05e31d86820df70
DrapNard <bastienorfao@gmail.com>
2023-08-05 18:23:18 +0200

    Add .gitignore

--- obj/Debug/net6.0-windows7.0/MainWindow.g.i.cs
+++ obj/Debug/net6.0-windows7.0/MainWindow.g.i.cs
@@ -184,7 +184,7 @@ namespace Pokémon_Infinite_Fusion_Launcher {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Pokémon Infinite Fusion Launcher;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Pokémon Infinite Fusion Launcher;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
